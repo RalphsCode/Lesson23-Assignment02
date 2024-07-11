6a640242384bfab1b2c730c56474e9aa72729dbd
Ralph's Code <RalphsCode@gmail.com>
2024-07-10 20:21:57 -0700

    Created a route for API to get all cupcakes, and added a link on the home page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template  
+from flask import Flask, render_template, jsonify 
 from flask_sqlalchemy import SQLAlchemy
 from models import db, connect_db, Cupcake
 # Need Flask to start a server
@@ -13,9 +13,22 @@ with app.app_context():
     connect_db(app)
     db.create_all()
 
+def serialize(cupcake):
+    return {
+        "id": cupcake.id,
+        "flavor": cupcake.flavor,
+        "rating": cupcake.rating,
+        "image": cupcake.image
+    }
 
 @app.route('/')
 def home():
     cupcakes = Cupcake.query.all()
     return render_template('home.html', cupcakes=cupcakes)
  
+@app.route('/api/cupcakes')
+def all_cupcakes():
+    cupcakes = Cupcake.query.all()
+    serialized = [serialize(cupcake) for cupcake in cupcakes]
+
+    return (jsonify(cupcakes=serialized), 200)
