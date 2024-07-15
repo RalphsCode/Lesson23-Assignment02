9c998a08c8c0b4ccc0269e6637d2d2aa8f84e2f2
Ralph's Code <RalphsCode@gmail.com>
2024-07-15 09:57:11 -0700

    Added code to process the add cupcake form, and used a JS function to POST the data to the API which adds the new cupcake to the database. Also added some bootstrap styling.

--- static/script.js
+++ static/script.js
@@ -1,8 +1,7 @@
 console.log('JS is running')
 
 
-
-async function getCupcakeInfo() {
+window.onload = async function getCupcakeInfo() {
     let response = await axios.get("/api/cupcakes");
     console.log("got:", response.data);
     console.log("flavor in function", response.data.cupcakes[0].flavor);
@@ -14,11 +13,41 @@ async function getCupcakeInfo() {
         let row = `<tr><td>${cake.flavor}</td><td>${cake.size}</td><td>${cake.rating}</td><td><img src=${cake.image} width=100px></td>`
         body_tag.innerHTML = body_tag.innerHTML + row;
     }
-    
-
     return cupcake_data }
 
-// window.addEventListener('load', getCupcakeInfo())
+// let the_cupcakes = getCupcakeInfo();
+
+button = document.getElementById("btn_new_cupcake")
+button.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    get_new_cupcake_data();
+}) // End addEventListener
 
 
-let the_cupcakes = getCupcakeInfo();
+
+async function send_data(new_cupcake) {
+    await axios.post("/api/cupcakes", new_cupcake, {
+        headers: {
+            'Content-Type': 'application/json' // Needed for the post query
+          }
+    })
+  .then(response => {
+    console.log(response.data); // Handle successful response
+    window.location.href = '/';
+    })
+  .catch(error => {
+    console.error(error); // Handle error
+  });
+}
+
+function get_new_cupcake_data() {
+    form_content = document.getElementById('form_new_cupcake');
+        const new_cupcake = {};
+        new_cupcake.flavor = $('#flavor').val();
+        new_cupcake.size = $('#size').val();
+        new_cupcake.rating = $('#rating').val();
+        new_cupcake.image = $('#image').val();
+        const new_cupcake_JSON = JSON.stringify(new_cupcake);
+        console.log('NEW CUPCAKE:', new_cupcake_JSON);
+        send_data(new_cupcake_JSON);
+    }  // END get_data()
